339445bdcf75914ee6a67f447fb73b6e539ad35c
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-01-29 14:59:08 +0100

    category icons

--- StxInsider/app/src/main/java/com/stxnext/stxinsider/constant/TeamCategories.java
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/constant/TeamCategories.java
@@ -7,8 +7,9 @@ import com.stxnext.stxinsider.model.TeamCatergoryHeader;
  */
 public class TeamCategories {
     public static TeamCatergoryHeader[] teams = new TeamCatergoryHeader[] {
-            new TeamCatergoryHeader().header("Meet the teams").footer("See what types of projects and what kinds of technologies we deal within an interactive tour through our company"),
-            new TeamCatergoryHeader().header("Data Mining Solutions").imagePath("/nopath").footer("Lorem ipsum"),
-            new TeamCatergoryHeader().header("Financial Anylysis Tools").imagePath("/nopath").footer("Lorem ipsum")
+            new TeamCatergoryHeader().header("Meet the teams").imagePath("teams/meet_the_teams.png")
+                    .footer("See what types of projects and what kinds of technologies we deal within an interactive tour through our company"),
+            new TeamCatergoryHeader().header("Data Mining Solutions").imagePath("teams/data_mining.png").footer("Lorem ipsum"),
+            new TeamCatergoryHeader().header("Financial Analysis Tools").imagePath("teams/financial_analysis.jpg").footer("Lorem ipsum")
     };
 }
--- StxInsider/app/src/main/java/com/stxnext/stxinsider/fragment/TeamCategoryFragment.java
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/fragment/TeamCategoryFragment.java
@@ -1,20 +1,29 @@
 package com.stxnext.stxinsider.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.stxnext.stxinsider.R;
 import com.stxnext.stxinsider.model.TeamCatergoryHeader;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by bkosarzycki on 22.01.16.
  */
 public class TeamCategoryFragment extends Fragment {
 
+    final String TAG = TeamCategoryFragment.class.getName();
     TeamCatergoryHeader teamCatergoryHeader;
 
     public TeamCategoryFragment teamCategoryHeader(TeamCatergoryHeader teamCatergoryHeader) {
@@ -28,6 +37,15 @@ public class TeamCategoryFragment extends Fragment {
 
         ((TextView)view.findViewById(R.id.fragment_team_header_main_header)).setText(teamCatergoryHeader.getHeader());
         ((TextView)view.findViewById(R.id.fragment_team_header_footer)).setText(teamCatergoryHeader.getFooter());
+        ImageView img = (ImageView)view.findViewById(R.id.fragment_team_header_image);
+        try {
+            InputStream file = getContext().getAssets().open(teamCatergoryHeader.getImagePath());
+            Drawable d = Drawable.createFromStream(file, null);
+            img.setImageDrawable(d);
+        } catch (IOException e) {
+            Log.e(TAG, "Cannot read image from assets: " + e.toString());
+        }
+
 
         return view;
     }
