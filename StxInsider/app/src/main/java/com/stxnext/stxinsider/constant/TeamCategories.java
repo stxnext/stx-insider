4a7864b65cb342a54067aa0ddff0075932730392
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-02-01 18:46:46 +0100

    dark theme

--- StxInsider/app/src/main/java/com/stxnext/stxinsider/constant/TeamCategories.java
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/constant/TeamCategories.java
@@ -10,10 +10,10 @@ public class TeamCategories {
     public static TeamCategoryHeader[] teams = new TeamCategoryHeader[] {
             new TeamCategoryHeader().header("Meet the teams").imagePath("teams/meet_the_teams.png")
                     .footer("See what types of projects and what kinds of technologies we deal within an interactive tour through our company")
-                    .background("teams/teams_backg.jpg"),
-            new TeamCategoryHeader().category(TeamCategory.MARKETING).header("Marketing").imagePath("teams/financial_analysis.jpg").footer("Lorem ipsum"),
-            new TeamCategoryHeader().category(TeamCategory.BANKING).header("Banking").imagePath("teams/financial_analysis.jpg").footer("Lorem ipsum"),
+                    .background("teams/teams_backg.png"),
+            new TeamCategoryHeader().category(TeamCategory.MARKETING).header("Marketing").imagePath("teams/financial_analysis.png").footer("Lorem ipsum"),
+            new TeamCategoryHeader().category(TeamCategory.BANKING).header("Banking").imagePath("teams/financial_analysis.png").footer("Lorem ipsum"),
             new TeamCategoryHeader().category(TeamCategory.DATA_MINING).header("Data Mining Solutions").imagePath("teams/data_mining.png").footer("Lorem ipsum"),
-            new TeamCategoryHeader().category(TeamCategory.FINANCIAL_ANALYSIS).header("Financial Analysis Tools").imagePath("teams/financial_analysis.jpg").footer("Lorem ipsum")
+            new TeamCategoryHeader().category(TeamCategory.FINANCIAL_ANALYSIS).header("Financial Analysis Tools").imagePath("teams/financial_analysis.png").footer("Lorem ipsum")
     };
 }
