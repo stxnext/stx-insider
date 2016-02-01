88c2ad2b92dba70f7ac2d70ae4193e32b2e575de
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-02-01 14:11:48 +0100

    add team item background

--- StxInsider/app/src/main/java/com/stxnext/stxinsider/view/TeamItemView.java
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/view/TeamItemView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.stxnext.stxinsider.R;
@@ -29,7 +30,9 @@ public class TeamItemView extends FrameLayout {
         this.item = item;
 
         TextView nameTextView = (TextView) this.findViewById(R.id.item_teams_list_header);
+        ImageView teamImageView = (ImageView) this.findViewById(R.id.item_teams_list_team_background);
         nameTextView.setText(item.getHeader());
+        teamImageView.setImageResource(android.R.drawable.ic_btn_speak_now);
 
         if (clickListener != null)
             this.setOnClickListener(clickListener);
