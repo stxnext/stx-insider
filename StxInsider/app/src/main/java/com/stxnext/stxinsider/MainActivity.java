b431ba7dbc7890c4b6cd259ee5532ca9c55d971e
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-02-03 18:06:54 +0100

    call a taxi

--- StxInsider/app/src/main/java/com/stxnext/stxinsider/MainActivity.java
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/MainActivity.java
@@ -1,16 +1,25 @@
 package com.stxnext.stxinsider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.activity_main_taxi_phone_no_tv)
+    TextView taxiPhoneNoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,4 +38,14 @@ public class MainActivity extends AppCompatActivity {
     public void onVisitUsClick(View v) {
         startActivity(new Intent(getApplicationContext(), MapActivity.class));
     }
+
+    @OnClick(R.id.activity_main_taxi_phone_no_tv)
+    public void callTaxiClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + taxiPhoneNoTextView.getText()));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) { return; }
+        startActivity(intent);
+    }
+
+
 }
