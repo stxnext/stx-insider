2c55b526455a33e1099bfd427c770231a5706e65
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-02-04 20:39:10 +0100

    ItemListActivity stub

---
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/ItemListActivity.java
@@ -0,0 +1,34 @@
+package com.stxnext.stxinsider;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.stxnext.stxinsider.R;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+        getSupportActionBar().setTitle("ITEMS");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+            finish();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
