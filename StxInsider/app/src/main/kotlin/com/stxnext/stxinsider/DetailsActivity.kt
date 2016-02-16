1ca628b2090d85a32787913aa5b60b4483ad2989
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2016-02-16 20:21:45 +0100

    dynamic details list fragment

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/DetailsActivity.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/DetailsActivity.kt
@@ -1,8 +1,12 @@
 package com.stxnext.stxinsider
 
+import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 
 import com.google.gson.Gson
@@ -27,6 +31,30 @@ class DetailsActivity<T> : AppCompatActivity() {
 
         mItem = Gson().fromJson<DetailsItem<T>>(intent.getStringExtra("item"), DetailsItem::class.java /*Team::class.java*/)
         bind(mItem!!)
+        replaceContentFragment()
+    }
+
+    class EmptyFragment : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            return inflater!!.inflate(R.layout.fragment_empty, container, false)
+        }
+    }
+
+    private fun replaceContentFragment() {
+
+        //val inflater = LayoutInflater.from(this);
+        //supportFragmentManager.
+        val transaction = fragmentManager.beginTransaction()
+        val contentFragment : Fragment = object : Fragment() {
+            override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+                return inflater!!.inflate(R.layout.activity_details_content_list, container, false)
+            }
+        }
+
+        transaction.replace(R.id.activity_details_content_fragment, contentFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+
     }
 
     private fun bind(item: DetailsItem<T>) {
