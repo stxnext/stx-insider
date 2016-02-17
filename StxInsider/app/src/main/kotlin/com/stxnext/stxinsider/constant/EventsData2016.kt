a9c6b0e5e0e17f7b01c19d4e6cec40e186f208b0
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2016-02-17 19:56:32 +0100

    set image source & image height dynamically

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/constant/EventsData2016.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/constant/EventsData2016.kt
@@ -9,12 +9,12 @@ import com.stxnext.stxinsider.view.model.DetailsItem
 object EventsData2016 {
 
     val data : List<DetailsItem<SpannableString>> = listOf(
-            DetailsItem<SpannableString>("Wacław Zalewski & Henk van Leussen", "Presentation additional info",
+            DetailsItem<SpannableString>("Wacław Zalewski & Henk van Leussen", "Presentation additional info", "teams/events/stx2016/row_0.jpg",
                     SpannableString("<b>Abstract:</b> <br/> <br/> As nearshoring and outsourcing are becoming increasingly important components of the modern economy, we would like to address opportunities and business value Central European partners can provide. We we'll also discuss the current practice and share our thoughts on the future of this type of cooperation. <br/> <br/> <b>Wacław <br/> <br/> Bio: <br/> <br/> </b> Wacław having the pleasure of leading the magnificent Business Research Team, my responsibilities include, but are not limited to, prospecting, lead generation, CRM management and finding a way to get in touch with decision makers you need. I'm also unable to write about myself in third person.  <br/> <br/> <b>Henk <br/> <br/>  Bio: </b> <br/> <br/>  In my job, VP of Business Development, I’m managing all phases of business development, business planning, prospecting, building C-level relationships, detailed presentations and negotiations to closing and follow-up. I’m well in improving (administrative) processes, have a creative mind, think in solutions and may well convince. <br/> <br/>")),
-            DetailsItem<SpannableString>("Name Surname", "Aditional text", SpannableString("Some spannable content <b> bold content </b>")),
-            DetailsItem<SpannableString>("Name Surname", "Aditional text", SpannableString("Some spannable content <b> bold content </b>")),
-            DetailsItem<SpannableString>("Name Surname", "Aditional text", SpannableString("Some spannable content <b> bold content </b>")),
-            DetailsItem<SpannableString>("Name Surname", "Aditional text", SpannableString("Some spannable content <b> bold content </b>"))
+            DetailsItem<SpannableString>("Name Surname", "Aditional text", "teams/events/stx2016/row_1.jpg", SpannableString("Some spannable content <b> bold content </b>")),
+            DetailsItem<SpannableString>("Name Surname", "Aditional text", "teams/events/stx2016/row_3.jpg", SpannableString("Some spannable content <b> bold content </b>")),
+            DetailsItem<SpannableString>("Name Surname", "Aditional text", "teams/events/stx2016/row_4.jpg", SpannableString("Some spannable content <b> bold content </b>")),
+            DetailsItem<SpannableString>("Name Surname", "Aditional text",  "teams/events/stx2016/row_5.jpg",SpannableString("Some spannable content <b> bold content </b>"))
 
     )
 }
