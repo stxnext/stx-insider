616ee4dc097e76a673d3d57b5c220ee372a5a36d
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2016-02-23 09:19:07 +0100

    portfolio categories data classes

---
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/constant/Categories.kt
@@ -0,0 +1,11 @@
+package com.stxnext.stxinsider.constant
+
+import com.stxnext.stxinsider.model.Category
+
+/**
+ * Created by Bartosz Kosarzycki on 23.02.16.
+ */
+object Categories {
+    @JvmField
+    val categoryList = arrayOf(Category.MOBILE, Category.WEB)
+}
