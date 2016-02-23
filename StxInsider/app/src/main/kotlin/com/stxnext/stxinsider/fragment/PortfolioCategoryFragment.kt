0a2e3d99793ab3da24a5371008e5f0e5b673e7eb
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2016-02-23 15:16:12 +0100

    @Suppress warnings in some cases

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/fragment/PortfolioCategoryFragment.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/fragment/PortfolioCategoryFragment.kt
@@ -31,6 +31,7 @@ class PortfolioCategoryFragment : Fragment() {
         return this
     }
 
+    @Suppress("SENSELESS_COMPARISON")
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_portfolio_layout, container, false)
 
