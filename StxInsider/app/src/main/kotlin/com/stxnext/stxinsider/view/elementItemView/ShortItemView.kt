79bbdc7bd854ca7fda67ec213b89d85479302c45
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2016-02-25 17:01:39 +0100

    decouple ShortItemView and TallItemView

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/view/elementItemView/ShortItemView.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/view/elementItemView/ShortItemView.kt
@@ -8,9 +8,9 @@ import com.stxnext.stxinsider.R
 /**
  * Created by bkosarzycki on 25.02.16.
  */
-class LongItemView(private val mContext: Context, attrs: AttributeSet?) : BaseItemView(mContext, attrs) {
+class ShortItemView(private val mContext: Context, attrs: AttributeSet?) : BaseItemView(mContext, attrs) {
 
     override fun addLayoutView(cont : Context) {
-        addView(LayoutInflater.from(cont).inflate(R.layout.item_portfolio_list, this, false))
+        addView(LayoutInflater.from(cont).inflate(R.layout.item_team_list, this, false))
     }
 }
