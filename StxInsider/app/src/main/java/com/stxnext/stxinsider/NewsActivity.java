8c1c78d128ff29dd656a1e33858fa77639b7e1e6
Łukasz Ciupa <losogitos@gmail.com>
2016-03-01 16:23:56 +0100

    Redirection to News Activity.

--- StxInsider/app/src/main/java/com/stxnext/stxinsider/NewsActivity.java
+++ StxInsider/app/src/main/java/com/stxnext/stxinsider/NewsActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
 /**
