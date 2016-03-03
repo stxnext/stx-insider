d00597016c74af62ca500b5741794bbf55f69898
Łukasz Ciupa <losogitos@gmail.com>
2016-03-03 16:09:13 +0100

    Map navigation.

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/MapActivity.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/MapActivity.kt
@@ -40,6 +40,7 @@ class MapActivity : AppCompatActivity() {
     internal val wifiPassTextView: TextView by bindView(R.id.activity_main_wifi_pass_tv)
     internal val wifiProgressBar: ProgressBar  by bindView(R.id.activity_main_wifi_connection_progressbar)
     internal val mainWifiOutLayout: LinearLayout  by bindView(R.id.activity_main_wifi_outer_layout)
+    internal val address : View by bindView(R.id.address);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +55,14 @@ class MapActivity : AppCompatActivity() {
         prepareMap()
         taxiPhoneNoTextView.setOnClickListener { v:View -> callTaxiClick() }
         mainWifiOutLayout.setOnClickListener { v:View -> connectToWifi() }
+        address.setOnClickListener { v:View -> navigate() }
+    }
+
+    private fun navigate() {
+        val intent = Intent(android.content.Intent.ACTION_VIEW, Uri.parse(
+                "http://maps.google.com/maps?daddr="
+                        + OFFICE_LOCATION.latitude + ", " + OFFICE_LOCATION.longitude))
+        startActivity(intent)
     }
 
     internal var wifiInitStarted: DateTime? = null
