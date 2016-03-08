b9682d979513b8459eb6ce9eef118186cbbe7a5f
Łukasz Ciupa <losogitos@gmail.com>
2016-03-08 17:24:05 +0100

    Office localization detection.

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/util/Location.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/util/Location.kt
@@ -6,6 +6,7 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.maps.model.LatLng
 
 /**
  * Created by Łukasz Ciupa on 08.03.2016.
@@ -13,12 +14,19 @@ import android.util.Log
 
 class Location(context: Context) {
 
+    val OFFICE_LOCATION = LatLng(52.3944957, 16.8936571)
+
     val locationManager : LocationManager;
     var listener : OnLocationListener? = null;
     val locationListener = object: LocationListener  {
-        override fun onLocationChanged(p0: Location?) {
-            Log.d(TAG, "On Location changed: " + p0)
-            listener?.onOfficeLocationDetected()
+        override fun onLocationChanged(location: Location?) {
+            Log.d(TAG, "On Location changed: " + location)
+            if (isLocationOffice(location))
+            {
+                listener?.onOfficeLocationDetected()
+                stopLookingForLocation()
+            }
+
         }
         override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
             throw UnsupportedOperationException()
@@ -49,6 +57,14 @@ class Location(context: Context) {
 
     public interface OnLocationListener {
         fun onOfficeLocationDetected()
+    }
+
+    private fun isLocationOffice(location: Location?) : Boolean {
+        if (location != null && location.latitude > (OFFICE_LOCATION.latitude - 0.1) && location.latitude < (OFFICE_LOCATION.latitude + 0.1)
+        && location.longitude > (OFFICE_LOCATION.longitude - 0.1) && location.longitude < (OFFICE_LOCATION.longitude + 0.1))
+            return true
+        else
+            return false
     }
 
 }
