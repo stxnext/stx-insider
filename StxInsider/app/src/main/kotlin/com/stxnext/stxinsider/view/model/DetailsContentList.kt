622bf1fd3b41396bf37ce70dbe5ddbcebdb67f69
Łukasz Ciupa <losogitos@gmail.com>
2016-03-24 16:37:07 +0100

    Ignore clicks for 1st event. Event numver information.

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/view/model/DetailsContentList.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/view/model/DetailsContentList.kt
@@ -3,4 +3,4 @@ package com.stxnext.stxinsider.view.model
 /**
  * Created by bkosarzycki on 16.02.16.
  */
-data class DetailsContentList(var data: List<DetailsContentListRow>)
+data class DetailsContentList(var data: List<DetailsContentListRow>, var listID : Int)
