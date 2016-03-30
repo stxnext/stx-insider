f4f8a3bb344aa3e43dcb3472917901fae8fa9d25
Łukasz Ciupa <losogitos@gmail.com>
2016-03-30 11:26:20 +0200

    Navigation.

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/ItemListActivity.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/ItemListActivity.kt
@@ -12,6 +12,7 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
 import butterknife.bindView
+import com.google.android.gms.maps.model.LatLng
 import com.google.gson.Gson
 import com.stxnext.stxinsider.adapter.SimpleItemListAdapter
 import com.stxnext.stxinsider.view.ListItemView
@@ -70,7 +71,7 @@ class ItemListActivity : AppCompatActivity() {
                         DetailsContentListRow("Key Note: Self-organizing teams - Team Maturity", "16:20 - 17:00 - Angel Medinilla"),
                         DetailsContentListRow("10 years Anniversary talk", "18:25 - 19:00 - Maciej Dziergwa"),
                         DetailsContentListRow("Break", "19:00 - 20:00"),
-                        DetailsContentListRow("Banquet", "20:00 - 02:00")).toList(), 1), replacingImagePath = null
+                        DetailsContentListRow("Banquet", "20:00 - 02:00")).toList(), 1, LatLng(52.3946831, 16.8940677), "Hotel Poznański - ul. Krańcowa 4, Luboń 62-657."), replacingImagePath = null
             )
             else if (position == 1)
 
@@ -90,7 +91,7 @@ class ItemListActivity : AppCompatActivity() {
                        DetailsContentListRow("Software Quality Visualization", "15:50 - 16:20 - Łukasz Koczwara"),
                        DetailsContentListRow("STX Next - 2016 plans", "16:20 - 17:00 - Maciej Dziergwa"),
                        DetailsContentListRow("Break", "17:00 - 18:00"),
-                       DetailsContentListRow("Banquet", "18:00 - 01:00")).toList(), 2), replacingImagePath = null
+                       DetailsContentListRow("Banquet", "18:00 - 01:00")).toList(), 2, LatLng(52.3946831, 16.8940677), "Hotel Poznański - ul. Krańcowa 4, Luboń 62-657."), replacingImagePath = null
                 )
             if (detailsItem != null) {
                 val detailsItemString = Gson().toJson(detailsItem)
--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/dialog/NavigationDialogFragment.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/dialog/NavigationDialogFragment.kt
@@ -3,8 +3,12 @@ package com.stxnext.stxinsider.dialog
 import android.app.Dialog
 import android.app.DialogFragment
 import android.app.FragmentManager
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.util.Log
+import com.google.android.gms.maps.model.LatLng
 import com.stxnext.stxinsider.R
 
 /**
@@ -13,13 +17,13 @@ import com.stxnext.stxinsider.R
 class NavigationDialogFragment : DialogFragment() {
 
     val ARG_MESSAGE : String = "message";
-    val ARG_DESTINATION: String = "actionName"
+    val ARG_DESTINATION: String = "destination"
     val TAG = "action_dialog"
 
-    fun showDialog(manager: FragmentManager?, message: String?, destination: String?) {
+    fun showDialog(manager: FragmentManager?, message: String?, destination: LatLng?) {
         var bundle : Bundle = Bundle();
         bundle.putString(ARG_MESSAGE, message)
-        bundle.putString(ARG_DESTINATION, destination)
+        bundle.putParcelable(ARG_DESTINATION, destination)
         arguments = bundle
         show(manager, TAG)
     }
@@ -30,12 +34,19 @@ class NavigationDialogFragment : DialogFragment() {
         if (arguments == null)
             throw RuntimeException("There are no arguments for dialog")
         val message : String = arguments.getString(ARG_MESSAGE)
-        val destination : String = arguments.getString(ARG_DESTINATION)
+        val destination : LatLng = arguments.getParcelable(ARG_DESTINATION)
+        Log.d(TAG, "Latitude: " + destination.latitude.toString())
         var builder : AlertDialog.Builder = AlertDialog.Builder(activity)
         builder.setCancelable(true)
-        builder.setMessage(message)
-        builder.setPositiveButton(getString(R.string.launch), { dialog, which -> dismiss()})
-        .setNeutralButton(getString(android.R.string.cancel).toUpperCase(), {dialog, which -> dismiss()})
+        .setTitle(R.string.launch_navigation)
+        .setMessage(message)
+        .setPositiveButton(getString(R.string.launch), { dialog, which ->
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(
+                    "http://maps.google.com/maps?daddr="
+                            + destination.latitude + ", " + destination.longitude))
+            startActivity(intent)
+        })
+        .setNegativeButton(getString(android.R.string.cancel).toUpperCase(), {dialog, which -> dismiss()})
         return builder.create()
     }
 }
