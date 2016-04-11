4ec310e1e6bdab29be214f2cf97dbdc82f8f6b64
Łukasz Ciupa <losogitos@gmail.com>
2016-04-11 13:27:26 +0200

    Team details elevation shadow.

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/TeamDetailsActivity.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/TeamDetailsActivity.kt
@@ -2,25 +2,34 @@ package com.stxnext.stxinsider
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CollapsingToolbarLayout
+import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.MenuItem
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 
 import com.google.gson.Gson
 import com.stxnext.stxinsider.model.SliderItem
 import butterknife.bindView
+import com.stxnext.stxinsider.util.Util
+import com.stxnext.stxinsider.util.convertDpToPixel
 import java.io.IOException
 
 class TeamDetailsActivity : AppCompatActivity() {
 
     val mHeaderTextView: TextView by bindView(R.id.activity_team_details_team_header)
+    val appBar: AppBarLayout by bindView(R.id.app_bar_layout)
+    val mCollapsingToolbarLayout: CollapsingToolbarLayout by bindView(R.id.activity_details_collapsingToolbar)
     val mSubtitle: TextView by bindView(R.id.subtitle)
     val mDescriptionTextView: TextView by bindView(R.id.activity_team_details_team_description)
     val mBackgroundImageView: ImageView by bindView(R.id.activity_details_header_image)
     val mToolbar: Toolbar by bindView(R.id.toolbar)
+    val header: LinearLayout by bindView(R.id.header)
     internal val TAG = TeamDetailsActivity::class.java.name
 
     var mTeam: SliderItem? = null
@@ -34,6 +43,7 @@ class TeamDetailsActivity : AppCompatActivity() {
 
         mTeam = Gson().fromJson<SliderItem>(intent.getStringExtra("item"), SliderItem::class.java)
         bind(mTeam!!)
+        addElevationAnimationWhenScroll()
     }
 
     private fun bind(item: SliderItem) {
@@ -49,6 +59,33 @@ class TeamDetailsActivity : AppCompatActivity() {
             Log.e(TAG, "Error creating team image: " + e.toString())
         }
 
+    }
+
+    private fun addElevationAnimationWhenScroll() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            appBar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+                override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                    val currentHeight = mCollapsingToolbarLayout.getHeight() + verticalOffset
+                    val startingElevationHeight: Float = 1.4f * ViewCompat.getMinimumHeight(mCollapsingToolbarLayout)
+                    Log.d(TAG, "staring height: " + startingElevationHeight)
+                    if (currentHeight < startingElevationHeight) {
+                        Log.d(TAG, "Toolbar collapsed. offset is: " + verticalOffset + " current toolbarHeight is:" + mCollapsingToolbarLayout.getHeight() + " where minimum toolbar height is: " + ViewCompat.getMinimumHeight(mCollapsingToolbarLayout))
+                        header.elevation = getElevationForOffset(currentHeight, ViewCompat.getMinimumHeight(mCollapsingToolbarLayout), startingElevationHeight)
+                    } else {
+                        Log.d(TAG, "Toolbar uncollapsed. offset is: " + verticalOffset + " current toolbarHeight is:" + mCollapsingToolbarLayout.getHeight() + " where minimum toolbar height is: " + ViewCompat.getMinimumHeight(mCollapsingToolbarLayout))
+                        header.elevation = Util().convertDpToPixel(0f, this@TeamDetailsActivity)
+                    }
+                }
+            })
+        }
+    }
+
+    private fun getElevationForOffset(currentHeight: Int, destinationHeight: Int, startingHeight: Float): Float {
+        val currentHeightDifference = currentHeight - destinationHeight
+        val heightRange = startingHeight - destinationHeight
+        val elevationLevel = 1 - (currentHeightDifference / heightRange)
+        val destinationElevation = 3f
+        return Util().convertDpToPixel(elevationLevel * destinationElevation, this@TeamDetailsActivity)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
