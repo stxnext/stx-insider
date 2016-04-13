ffa32d4c8f72b0e5697004cbe63c6cce8c76dd38
Łukasz Ciupa <losogitos@gmail.com>
2016-04-13 10:06:07 +0200

    Refactor of ItemView bind moved to child.

--- StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/view/elementItemView/ShortItemView.kt
+++ StxInsider/app/src/main/kotlin/com/stxnext/stxinsider/view/elementItemView/ShortItemView.kt
@@ -1,9 +1,15 @@
 package com.stxnext.stxinsider.view.elementItemView
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
 import com.stxnext.stxinsider.R
+import com.stxnext.stxinsider.model.SliderItem
+import java.io.IOException
 
 /**
  * Created by bkosarzycki on 25.02.16.
@@ -12,5 +18,28 @@ class ShortItemView(private val mContext: Context, attrs: AttributeSet?, viewTyp
 
     override fun addLayoutView(cont : Context, viewType: Int) {
         addView(LayoutInflater.from(cont).inflate(R.layout.item_team_list, this, false))
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    override fun bind(item: SliderItem, position: Int, clickListener: OnClickListener?,
+                      seeMoreListener: OnItemViewSeeMoreClickListener?, itemClicked: Boolean) {
+        this.item = item
+        val nameTextView = findViewById(R.id.item_teams_list_header) as TextView
+        val teamImageView = findViewById(R.id.item_teams_list_team_background) as ImageView
+        //val titleTextView =  findViewById(R.id.title) as TextView;
+        val description = findViewById(R.id.description) as TextView
+        nameTextView.text = item.header
+
+        try {
+            val file = context.assets.open(item.imagePath)
+            teamImageView.setImageDrawable(Drawable.createFromStream(file, null))
+        } catch (e: IOException) {
+            Log.e(TAG, "Error creating team image: " + e.toString())
+        }
+        description.text = item.description
+
+        if (clickListener != null) {
+            this.setOnClickListener(clickListener)
+        }
     }
 }
